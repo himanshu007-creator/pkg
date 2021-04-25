1d106be00a1c259e21e31a180fb41cb712409860
Himanshu <addyjeridiq@gmail.com>
2021-04-25 14:48:20 +0530

    Add files via upload

---
+++ noobcode/greeting.py
@@ -0,0 +1,10 @@
+import random
+
+
+def thought():
+    a = "be your own voice"
+    b = "be awesome"
+    c = "one day at a time"
+    d = "aham brahmasmi!"
+    l = [a, b, c, d]
+    print(random.choice(l))
